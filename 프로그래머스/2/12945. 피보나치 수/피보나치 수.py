782fc52e7360080d1fbfde0b45a04960418ab235
sydney0716 <81623797+sydney0716@users.noreply.github.com>
2026-01-10 16:08:49 +0900

    [level 2] Title: 피보나치 수, Time: 396.68 ms, Memory: 455 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,16 @@
+def fibo(n):
+    if n < 2:
+        return n
+    
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    
+    return dp[n]
+    
+
+def solution(n):
+    answer = fibo(n) % 1234567
+    return answer
